e00e45dd5cd118bd441b82524a61934520e4da88
teneple <alexdavidson697@gmail.com>
2025-12-05 21:30:14 +1100

    feat(mainloop): add program mainloop structure

--- src/statistics-app.cpp
+++ src/statistics-app.cpp
@@ -1,7 +1,16 @@
 #include "statistics-app/statistics-app.hpp"
 #include "statistics-app/statistics-lib.hpp"
-#include <print>
+#include "statistics-app/menu-utils.hpp"
 
 void run() {
-	std::println("{}", add(2, 3));
+    const std::uint64_t EXIT_STATE{ 13 };
+    printWelcomeMessage();
+    
+    std::uint64_t usrMainMenuOption{ 0 };
+    do {
+        printMainMenu();
+        usrMainMenuOption = getUsrMenuOption();
+        handleMainMenuInput(usrMainMenuOption);
+    } while (usrMainMenuOption != EXIT_STATE);
+    printExitMessage();
 }
